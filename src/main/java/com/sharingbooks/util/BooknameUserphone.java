4a5071c7f7c7fe983cc51fa72b46e14583d35e64
cobaltg <zhaofuyu0606@gmail.com>
2017-08-06 01:44:53 +0800

    添加了Util包

---
+++ src/main/java/com/sharingbooks/util/BooknameUserphone.java
@@ -0,0 +1,21 @@
+package com.sharingbooks.util;
+
+public class BooknameUserphone {
+	private String bookName;
+	
+	private String userPhone;
+	
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	
+}
